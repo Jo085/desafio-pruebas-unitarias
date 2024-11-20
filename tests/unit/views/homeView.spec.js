8779d1d992e6070667e3426acfe2a2c6509c6464
Josefa <jocastro085@gmail.com>
2024-11-20 01:33:17 -0300

    cambio de shallowMount por mount

--- tests/unit/views/homeView.spec.js
+++ tests/unit/views/homeView.spec.js
@@ -1,10 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HomeView from '@/views/HomeView.vue'
 
 describe('Componente HomeView.vue', () => {
    test('Validación de match con el snapshot', () =>{
     // Selección del sujeto de prueba
-     const wrapper = shallowMount(HomeView)
+     const wrapper = mount(HomeView)
      // Aserción 
      expect(wrapper.html()).toMatchSnapshot()
    })
